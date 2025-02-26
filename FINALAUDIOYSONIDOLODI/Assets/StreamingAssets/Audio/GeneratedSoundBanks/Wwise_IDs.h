bf9dc36df432f4e840b4d11390c8a9ab37612734
delfisita <131388243+delfisita@users.noreply.github.com>
2025-02-26 12:34:29 -0300

    FINAL UPDATE

---
+++ FINALAUDIOYSONIDOLODI/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,39 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_329604__INSPECTORJ__FOOTSTEPS_MUDDY_C = 2151526124U;
+        static const AkUniqueID PLAY_497121__SONIDISTAPO__AMBIENTE_1312_PLAZA_DE_LA_DIGNIDAD_04_YA_VAN_A_VER_LAS_BALAS_QUE_NOS_TIRARON_VAN_A_VOLVER = 3254981665U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAINSOUNDBANK = 534561221U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
